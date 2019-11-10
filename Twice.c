44bda580425463287e94b3b58dae8950222aee1e
Zara Soleymanzadeh <55151105+zara64@users.noreply.github.com>
2019-11-10 13:22:33 +0100

    Update Twice.c

--- Twice.c
+++ Twice.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
-void Twice( int arr1, int size)
+int * Twice( int arr1[], int n)
 {
 
-	int *ip=null;
-	int repp;
-	for (int i = 0; i < size; ++i)
+	int *p1= NULL;
+	int RepNum=0;
+	
+	for (int i = 0; i < n; i++)
 	{
-		repp=arr1[i];
-		for (int j = i+1; j < size; ++j)
+		RepNum = arr1[i];
+		for (int j = i+1; j < n; j++)
 		{
-			if (arr1[j] == repp)
+			if (arr1[j] == RepNum)
 			{
-				ip= &arr1[j];
+				p1= &arr1[j];
 				break;
-				
 			}
 			
 		}
-	if(ip != null)
+	if(p1 != NULL)
 	{
 		break;
 	}
 
 	}
-	printf("%d\n",*ip);
+	if( p1 != NULL)
+	{
+		return p1;}
+	else
+	{	
+		return NULL;
+	}
+
+	
 	
 }
 
@@ -32,18 +40,12 @@ void Twice( int arr1, int size)
 
 int main(int argc, char const *argv[])
 {
-	int array1[]={1,2,3,4,5,6,7,9,4,3}
+	int array1[]={1,2,3,4,5,6,7,9,4,3};
 	int size=10;
-	/*printf("please enter how many numbers you want to enter : \n");
-	scanf("%d",&size);
+	
 
-	for (int i = 0; i < size; i++)
-	{
-		scanf("%d",&array1[i]);
-	}
-	*/
-
-	Twice(array1,size);
+	int * res=Twice(array1,size);
+	printf("%p\n", res );
 
 	return 0;
 }
