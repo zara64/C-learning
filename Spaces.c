248f4710a4c6fb693dc7b662585b7768a77a1b0c
Zara Soleymanzadeh <55151105+zara64@users.noreply.github.com>
2019-11-10 17:21:40 +0100

    Update Spaces.c

--- Spaces.c
+++ Spaces.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 
-char * noSpace(char string1[300])
-{
-	char string2[300];
-	int c=0, d=0;
-	while (string1[c] != '\0')
-	{
-		if(string1[c] != ' ')
-		{
-			string2[d]=string1[c];
-			d++;
-		}
-		
-	c++;
 
-	}
-	return &string2;
-}
 int main(int argc, char const *argv[])
 {
 	char snt[300];
+	char string2[300];
+	int c=0, d=0;
+
 	printf("Please enter a sentence :\n");
 	fscanf(stdin,"%s",snt);
+
 	int i=0;
 	while(snt[i] != '\0'){
-		if(snt[i] == ' ');
-		i++;
-		//snt[i]='';
+
+		if(snt[i] == ' ')
+		{ 
+			i++;
+		}
+		else
+		{
+			string2[d]=snt[c];
+			d++;
+		}
+	c++;
 	}
 
-	char *ptrn = noSpace(snt);
+
 
 	printf("the number of spaces in sentence is %d \n",i);
-	printf("the sentence without space is %s ", ptrn);
+	printf("the sentence without space is %s ", string2);
 
 	return 0;
 }
