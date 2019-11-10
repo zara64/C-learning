626c083b7752e0e62500b07ef5daee8c6bbcbf2f
Zara Soleymanzadeh <55151105+zara64@users.noreply.github.com>
2019-11-10 14:38:09 +0100

    Create HelloX.c

---
+++ HelloX.c
@@ -0,0 +1,9 @@
+
+int main(int argc, char const *argv[])
+{
+	char Name[20];
+	printf("Please enter your Name!\n");
+	fscanf(stdin,"%s", Name);
+	printf("Hello and welcome dear %s .\n", Name);
+	return 0;
+}
