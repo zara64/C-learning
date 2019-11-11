23bfdd6bef763fb0f8653c537ee9c6df2d58ed05
Zara Soleymanzadeh <55151105+zara64@users.noreply.github.com>
2019-11-11 15:11:18 +0100

    Update PointInSpace.c

--- PointInSpace.c
+++ PointInSpace.c
@@ -2,18 +2,19 @@
 
 struct point
 {
-	int x;
-	int y;
+	double x;
+	double y;
 };
 
 int main(int argc, char const *argv[])
 {
 	double x,y;
-	printf("Please enter the point's length\n");
-	scanf("%ld\n",x);
 
-	printf("Please enter the point's width\n");
-	scanf("%ld\n",y);
+	printf("Enter the dimension of your coordination like x;y : \n");
+	scanf("%lf;%lf",&x,&y);
+
 	struct point p = {x,y};
+
+	//printf("x = %3.2lf, y = %3.2lf",p.x ,p.y );
 	return 0;
 }
